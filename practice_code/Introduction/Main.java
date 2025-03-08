28b759effb75058953324c5d48cba7c5cb2a82e7
Codingpro02 <dhruvbajaj@mac.home>
2025-03-07 21:32:31 -0500

    new file added

---
+++ practice_code/Introduction/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main (String [] args){
+        // the reason why static is with access modifier public is because we want
+        // run the code without making an object of it 
+        System.out.println("Hello World!!");
+    }
+}
